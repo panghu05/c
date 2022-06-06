938124f78ed0cdaf17efd37ac8cbc9a127b3a74e
libowen <5406160@qq.com>
2022-06-06 10:43:04 +0800

    2

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -2,5 +2,7 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("1 = " + 1);
         System.out.println("1 = " + 2);
+
+        System.out.println("5465");
     }
 }
